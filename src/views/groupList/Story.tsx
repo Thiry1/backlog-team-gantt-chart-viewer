697fb329709610eb877253625b2193433811fdcc
thiry <thiry@five-seven.net>
2019-03-06 18:29:45 +0900

    storybookのエラー修正

--- src/views/groupList/Story.tsx
+++ src/views/groupList/Story.tsx
@@ -1,19 +1,25 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
+import { MemoryRouter as Router } from "react-router-dom";
 import { Group } from "../../client/backlog/types";
 import { GroupList, GroupListProps } from "./groupList";
-
 const props: GroupListProps = {
   groups: [
     {
       id: 1,
+      name: "hoge",
     } as Group,
     {
       id: 2,
+      name: "fuga",
     } as Group,
   ],
 };
 
 storiesOf("グループリスト", module).add("グループリストを表示できる", () => {
-  return <GroupList {...props} />;
+  return (
+    <Router>
+      <GroupList {...props} />
+    </Router>
+  );
 });
