b49742b840cf3b414e46edfc7f1174f784007654
thiry <thiry@five-seven.net>
2019-03-19 16:17:57 +0900

    basic認証追加

--- src/config/server/serverConfig.ts
+++ src/config/server/serverConfig.ts
@@ -10,6 +10,23 @@ export interface ServerConfig {
      */
     apiKey: string;
   };
+  /**
+   * ベーシック認証設定.
+   */
+  basicAuth: {
+    /**
+     * 認証を有効にするか.
+     */
+    enabled: boolean;
+    /**
+     * ベーシック認証の ID.
+     */
+    id: string;
+    /**
+     * ベーシック認証のパスワード.
+     */
+    password: string;
+  };
 }
 
 export const serverConfig: ServerConfig = require("config"); // サーバーサイドでは全てのコンフィグを読み込んでも良い.
--- src/server/express/index.ts
+++ src/server/express/index.ts
@@ -1,12 +1,23 @@
 import * as express from "express";
 import * as path from "path";
+import { serverConfig } from "../../config/server/serverConfig";
 import { apiRouter } from "./routes/api";
 import { pageRouter } from "./routes/pages";
+const expressBasicAuth = require("basic-auth-connect");
 
 export const initializeExpress = () => {
   const app = express();
 
   app.set("view engine", "ejs");
+
+  if (serverConfig.basicAuth.enabled) {
+    app.use(
+      expressBasicAuth(
+        serverConfig.basicAuth.id,
+        serverConfig.basicAuth.password,
+      ),
+    );
+  }
 
   app.use(pageRouter);
   app.use(apiRouter);
